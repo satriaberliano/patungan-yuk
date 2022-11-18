0ec6f102461b62566787414244a5fd9a926c0834
Muhammad Bachtiar <101853715+muhammadbachtiar@users.noreply.github.com>
2022-11-18 18:01:16 +0700

    Delete src directory

--- src/contexts/TabContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
- 
-const TabContext = React.createContext();
- 
-export const LocaleProvider = TabContext.Provider;
-export const LocaleConsumer = TabContext.Consumer;
- 
-export default TabContext;
