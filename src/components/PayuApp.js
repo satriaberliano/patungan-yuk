da514fe29976847b351fec3b630911d39bc721e2
AldoWijaya27 <aldo.wijaya2012@students.unila.ac.id>
2022-11-26 22:28:27 +0700

    fixed footer dan button dashboard

--- src/components/PayuApp.js
+++ src/components/PayuApp.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from "../pages/Home";
-import AppHeader from "./AppHeader";
-import DetailPage from "../pages/DetailPage";
-import AddNewPatunganPage from "../pages/AddNewPatunganPage";
-import { rootPath, DetailPatunganPath, AddNewPatunganPath, InfoPath, LoginPath, RegisterPath, AboutPath, AddAnggotaPatunganPath, AddKegiatanPatunganPath, AddJumlahPatunganAnggotaPath, EditKegiatanPath, AddUangPatunganPath, ChangeKegiatanPath } from '../routes';
-import InfoPage from "../pages/InfoPage";
-import LoginPage from "../pages/LoginPage";
-import RegisterPage from "../pages/RegisterPage";
-import AboutPage from "../pages/AboutPage";
-import AddAnggotaPatunganPage from "../pages/AddAnggotaPatunganPage";
-import AddKegiatanPatunganPage from "../pages/AddKegiatanPatunganPage";
-import AddJumlahPatunganAnggotaPage from "../pages/AddJumlahPatunganAnggotaPage";
-import EditKegiatanPage from "../pages/EditKegiatanPage";
-import AddUangPatunganPage from "../pages/AddUangPatunganPage";
-import ChangeKegiatanPatunganPage from "../pages/ChangeKegiatanPatunganPage";
+import Home from '../pages/Home';
+import AppHeader from './AppHeader';
+import DetailPage from '../pages/DetailPage';
+import AddNewPatunganPage from '../pages/AddNewPatunganPage';
+import {
+  rootPath,
+  DetailPatunganPath,
+  AddNewPatunganPath,
+  InfoPath,
+  LoginPath,
+  RegisterPath,
+  AboutPath,
+  AddAnggotaPatunganPath,
+  AddKegiatanPatunganPath,
+  AddJumlahPatunganAnggotaPath,
+  EditKegiatanPath,
+  AddUangPatunganPath,
+  ChangeKegiatanPath,
+} from '../routes';
+import InfoPage from '../pages/InfoPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import AboutPage from '../pages/AboutPage';
+import AddAnggotaPatunganPage from '../pages/AddAnggotaPatunganPage';
+import AddKegiatanPatunganPage from '../pages/AddKegiatanPatunganPage';
+import AddJumlahPatunganAnggotaPage from '../pages/AddJumlahPatunganAnggotaPage';
+import EditKegiatanPage from '../pages/EditKegiatanPage';
+import AddUangPatunganPage from '../pages/AddUangPatunganPage';
+import ChangeKegiatanPatunganPage from '../pages/ChangeKegiatanPatunganPage';
+import AppFooter from './AppFooter';
 
-function PayuApp(){
-  return(
-    <div className="app-container">
+function PayuApp() {
+  return (
+    <div className='app-container'>
       <header>
         <AppHeader />
       </header>
@@ -26,19 +41,40 @@ function PayuApp(){
         <Routes>
           <Route path={rootPath} element={<Home />}></Route>
           <Route path={DetailPatunganPath} element={<DetailPage />}></Route>
-          <Route path={AddNewPatunganPath} element={<AddNewPatunganPage />}></Route>
+          <Route
+            path={AddNewPatunganPath}
+            element={<AddNewPatunganPage />}
+          ></Route>
           <Route path={InfoPath} element={<InfoPage />}></Route>
           <Route path={LoginPath} element={<LoginPage />}></Route>
           <Route path={RegisterPath} element={<RegisterPage />}></Route>
           <Route path={AboutPath} element={<AboutPage />}></Route>
-          <Route path={AddAnggotaPatunganPath} element={<AddAnggotaPatunganPage />}></Route>
-          <Route path={AddKegiatanPatunganPath} element={<AddKegiatanPatunganPage />}></Route>
-          <Route path={AddJumlahPatunganAnggotaPath} element={<AddJumlahPatunganAnggotaPage />}></Route>
+          <Route
+            path={AddAnggotaPatunganPath}
+            element={<AddAnggotaPatunganPage />}
+          ></Route>
+          <Route
+            path={AddKegiatanPatunganPath}
+            element={<AddKegiatanPatunganPage />}
+          ></Route>
+          <Route
+            path={AddJumlahPatunganAnggotaPath}
+            element={<AddJumlahPatunganAnggotaPage />}
+          ></Route>
           <Route path={EditKegiatanPath} element={<EditKegiatanPage />}></Route>
-          <Route path={AddUangPatunganPath} element={<AddUangPatunganPage />}></Route>
-          <Route path={ChangeKegiatanPath} element={<ChangeKegiatanPatunganPage />}></Route>
+          <Route
+            path={AddUangPatunganPath}
+            element={<AddUangPatunganPage />}
+          ></Route>
+          <Route
+            path={ChangeKegiatanPath}
+            element={<ChangeKegiatanPatunganPage />}
+          ></Route>
         </Routes>
       </main>
+      <footer>
+        <AppFooter />
+      </footer>
     </div>
   );
 }
