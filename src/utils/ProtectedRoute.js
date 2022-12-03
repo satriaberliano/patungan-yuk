3f0f136974d61edfffbad8412bde40bac375c730
hifkiyuda <hifkiyudapratama001@gmail.com>
2022-12-03 17:06:28 +0700

    fixed tab index

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -9,3 +9,15 @@ function ProtectedRoute(props) {
 }
 
 export default ProtectedRoute;
+
+// import { Navigate } from "react-router-dom";
+// import { auth } from "../config/firebase-config";
+
+// function ProtectedRoute(props){
+//   const user = auth.currentUser;
+
+//   if(!user) return <Navigate to='/info' />;
+//   return props.children;
+
+// }
+// export default ProtectedRoute;
