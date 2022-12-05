8efe097cbb0ff64387deca08857216966b20a834
hifkiyuda <hifkiyudapratama001@gmail.com>
2022-12-05 21:39:32 +0700

    add env

--- src/globals/firebase-config.js
+++ src/globals/firebase-config.js
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbvVMbKNY814VtWI3_XX1hwiMw2GZ8-Kg',
-  authDomain: 'patungan-yuk-fd0f9.firebaseapp.com',
-  databaseURL: 'https://patungan-yuk-fd0f9-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'patungan-yuk-fd0f9',
-  storageBucket: 'patungan-yuk-fd0f9.appspot.com',
-  messagingSenderId: '816852750018',
-  appId: '1:816852750018:web:1232b416f27bce1d1eb55b',
-  measurementId: 'G-K5W0PPX6ZQ',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
